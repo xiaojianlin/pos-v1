0a23b11783b4d221730c11522fae4b8726821d05
xxiao <843814555@qq.com>
2014-12-03 17:31:53 -0500

    pos change getallnumber

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -9,25 +9,22 @@ function getallnumbers(inputs){
   var itemNumber = [];
   for ( var j=0 ; j<loadAllItems().length ; j++){
     itemNumber[j] = 0;
+    
   }
   for (var i=0 ; i<inputs.length ; i++){
     var input = inputs[i].split('-');
-    if (input.length ===  2){
-      for ( var a=0 ; a<loadAllItems().length ; a++){
-        if(input[0] === loadAllItems()[a].barcode){
-          itemNumber[a] += parseInt(input[1]) ;
-          break;
+    itemNumber = getnumber (itemNumber,input) ;
+  }
+  return itemNumber;
+}
+
+
+function getnumber (itemNumber,input){
+  for ( var a=0 ; a<loadAllItems().length ; a++){
+    if(input[0] === loadAllItems()[a].barcode){
+      (input.length ===  2) ? (itemNumber[a] += parseInt(input[1])):
+                              (itemNumber[a]++) ;
         }
-      }
-    }
-    else{
-      for ( var a=0 ; a<loadAllItems().length ; a++){
-        if(input[0] === loadAllItems()[a].barcode){
-          itemNumber[a]++;
-          break;
-        }
-      }
-    }
   }
   return itemNumber;
 }
@@ -47,16 +44,16 @@ function getallmenu(allnumbers){
 
 function getmenu1(allnumbers){
   var menu1 = '' ;
-  for ( var i=0 ; i<allnumbers.length ; i++)
-    {if (allnumbers[i]!=0){
-      var number = 0 ;
+  var number = 0 ;
+  for ( var i=0 ; i<allnumbers.length ; i++){
+      if (allnumbers[i]!=0){
       load(i) ? (number = allnumbers[i] - parseInt(allnumbers[i]/3)) :
                 (number = allnumbers[i] ) ;
       menu1 += '名称：' + loadAllItems()[i].name +
                 '，数量：' +allnumbers[i] + loadAllItems()[i].unit +
                 '，单价：' + loadAllItems()[i].price.toFixed(2) +
                 '(元)，小计：' + (loadAllItems()[i].price*number).toFixed(2) + '(元)\n' ;
-     }
+      }
     }
   return menu1 ;
 }
@@ -98,5 +95,5 @@ function getmenu3(allnumbers){
     }
   }
   return menu3 = '总计：' + allsum.toFixed(2) + '(元)\n' +
-  '节省：' + loadsum.toFixed(2) + '(元)\n' ;
+                 '节省：' + loadsum.toFixed(2) + '(元)\n' ;
 }
